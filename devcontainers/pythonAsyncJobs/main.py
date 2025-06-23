370f242f3e23e0ac3b6e5faa0f6a61e8474b937e
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2025-06-22 20:04:27 -0500

    feat: update jeje

--- devcontainers/pythonAsyncJobs/main.py
+++ devcontainers/pythonAsyncJobs/main.py
@@ -1,5 +1,5 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 import redis
 import time
 from pydantic import BaseModel
@@ -10,6 +10,15 @@ app = FastAPI()
 r = redis.Redis(host='cache', port=6379, db=0)
 queue = Queue(connection=r)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 class Event(BaseModel):
     event: str
 
@@ -19,9 +28,17 @@ def process_event(event: str):
     print(f"Event processed: {event}")
 
 @app.post("/events/")
-def create_event(event: Event):
-    job = queue.enqueue(process_event, event.event)
-    return ({"job_id": job.id, "status": "queued"}), 200
+async def create_event(event: Request):
+    try:
+        body = await event.json()
+        print(f"Received event: {body}")
+        headers = event.headers
+        print(f"Headers: {headers}")
+    except Exception as e:
+        print(f"Error parsing JSON: {e}")
+    finally:
+        job = queue.enqueue(process_event, "push")
+        return ({"job_id": job.id, "status": "queued"}), 200
 
 if __name__ == "__main__":
     uvicorn.run("main:app", log_level="info")
